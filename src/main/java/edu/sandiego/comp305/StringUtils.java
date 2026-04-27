880fff6df91885bf52e4d00e37f367adf6d79640
Richard Ngo <richardno@sandiego.edu>
2026-04-26 22:04:45 -0700

    TDD P3: Created the rest of the method, so that all tests passed.

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -2,9 +2,23 @@ package edu.sandiego.comp305;
 
 public class StringUtils {
     public String reverseString(String originalString) {
+
         if (originalString.isEmpty()) {
             return "";
         }
-        return "";
+
+        char[] characters = originalString.toCharArray();
+        int leftIndex = 0;
+        int rightIndex = characters.length - 1;
+
+        while (leftIndex < rightIndex){
+            char tempCharacter = characters[leftIndex];
+            characters[leftIndex] = characters[rightIndex];
+            characters[rightIndex] = tempCharacter;
+            leftIndex++;
+            rightIndex--;
+        }
+
+        return new String(characters);
     }
 }
