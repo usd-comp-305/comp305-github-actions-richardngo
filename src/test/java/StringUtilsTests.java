89b4677558f1d3f6a543a25cce640c1e476761d1
Richard Ngo <richardno@sandiego.edu>
2026-04-26 22:21:26 -0700

    FIX: checkstyle errors gets rid of BeforeEach in tests.

--- src/test/java/StringUtilsTests.java
+++ src/test/java/StringUtilsTests.java
@@ -1,21 +1,13 @@
 import edu.sandiego.comp305.StringUtils;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringUtilsTests {
-    private StringUtils tool;
-
-    @BeforeEach
-    void initialize(){
-        tool = new StringUtils();
-    }
-
     @Test
     void reverseString_emptyString(){
         final String originalString = "";
-        final String reversedString = tool.reverseString(originalString);
+        final String reversedString = StringUtils.reverseString(originalString);
 
         assertEquals(originalString, reversedString);
     }
@@ -23,7 +15,7 @@ public class StringUtilsTests {
     @Test
     void reverseString_singleLetter(){
         final String originalString = "a";
-        final String reversedString = tool.reverseString(originalString);
+        final String reversedString = StringUtils.reverseString(originalString);
 
         assertEquals(originalString, reversedString);
     }
@@ -31,7 +23,7 @@ public class StringUtilsTests {
     @Test
     void reverseString_twoLetter(){
         final String originalString = "ab";
-        final String reversedString = tool.reverseString(originalString);
+        final String reversedString = StringUtils.reverseString(originalString);
         final String expectedString = "ba";
 
         assertEquals(expectedString, reversedString);
@@ -40,7 +32,7 @@ public class StringUtilsTests {
     @Test
     void reverseString_threeLetter(){
         final String originalString = "abc";
-        final String reversedString = tool.reverseString(originalString);
+        final String reversedString = StringUtils.reverseString(originalString);
         final String expectedString = "cba";
 
         assertEquals(expectedString, reversedString);
