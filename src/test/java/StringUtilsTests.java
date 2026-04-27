a10e303f7d33d42a2adcf8827de6582fe1be522f
Richard Ngo <richardno@sandiego.edu>
2026-04-26 21:46:52 -0700

    TDD P1: created the method stub of reverseString and the basic tests for reverseString (ALL FAILED)

---
+++ src/test/java/StringUtilsTests.java
@@ -0,0 +1,45 @@
+import edu.sandiego.comp305.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringUtilsTests {
+    private StringUtils tool;
+
+    @BeforeEach
+    void initialize(){
+        StringUtils tool = new StringUtils();
+    }
+
+    @Test
+    void reverseString_emptyString(){
+        String originalString = "";
+        String reversedString = tool.reverseString("originalString");
+        assertEquals(originalString, reversedString);
+    }
+
+    @Test
+    void reverseString_singleLetter(){
+        String originalString = "a";
+        String reversedString = tool.reverseString("originalString");
+        assertEquals(originalString, reversedString);
+    }
+
+    @Test
+    void reverseString_twoLetter(){
+        String originalString = "ab";
+        String reversedString = tool.reverseString("originalString");
+        String expectedString = "ba";
+        assertEquals(expectedString, reversedString);
+    }
+
+    @Test
+    void reverseString_threeLetter(){
+        String originalString = "abc";
+        String reversedString = tool.reverseString("originalString");
+        String expectedString = "cba";
+        assertEquals(expectedString, reversedString);
+    }
+
+}
